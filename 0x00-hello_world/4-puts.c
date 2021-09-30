816b775ef6c55586ff3cb556e198ffddf8e5aff0
diegojojoayandun <3922@holbertonschool.com>
2021-09-30 05:37:10 -0700

    add C file to use PUT function to print, to repo

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point, print phrase usin the put command
+ *
+ * Return: 0 if was success or any other number if not
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
