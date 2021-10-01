9338180225d05866083971399f4d482b1bd2cd6a
diegojojoayandun <3922@holbertonschool.com>
2021-10-01 16:44:18 -0700

    task 2 to repo

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 /** 
- * main - prints the alphabet using the putchar function 
+ * main - prints the alphabet using the putchar function
  * Return: 0 if success witout any error
  */
 int main(void)
-{
-	char letter;
+{      
+        char letter;
         for (letter = 'a'; letter <= 'z'; letter++)
-         {    
-                 putchar(letter);	
-         }	
+        {    
+                putchar(letter);
+        }	
 	putchar('\n');
 	return (0);
 }
