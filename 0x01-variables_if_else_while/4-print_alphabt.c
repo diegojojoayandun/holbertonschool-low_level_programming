734c04308118c6530ee9c7edca5ad1a93377a37e
diegojojoayandun <3922@holbertonschool.com>
2021-10-01 17:07:02 -0700

    task4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
 
-/*
+/**
  * main - prints the alphabet using the putchar function
  * avoid the q and e letters
- *
  * Return: 0 if success witout any error
  */
 
 int main(void)
 {
-  char letter;
+        char letter;
 
-  for (letter = 'a'; letter <= 'z'; letter++)
-    {
-      if ((letter != 'q') && (letter != 'e'))
-      {
-	putchar(letter);
-      }
-    }
-
-  putchar('\n');
-  return (0);
+        for (letter = 'a'; letter <= 'z'; letter++)
+        {
+                if ((letter != 'q') && (letter != 'e'))
+                {
+                        putchar(letter);
+                }
+                
+        }
+        putchar('\n');
+        return (0);
 }
