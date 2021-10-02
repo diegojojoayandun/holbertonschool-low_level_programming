cab02a7221603be5d2e14c4160da3e21b83fb7a6
diegojojoayandun <91098381+diegojojoayandun@users.noreply.github.com>
2021-10-01 22:15:56 -0500

    update file

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - prints the alphabet in lowercase,
+ * main - prints a-z,
  * followed by a new line
- * Return: Always 0 (Success)
+ * Return:0 if success
  */
 int main(void)
 {
-        char ch;
+	char ch;
 
-        for (ch = 'a'; ch <= 'z'; ch++)
-        {
-                putchar(ch);
-        }
-        putchar('\n');
-        return (0);                                
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
 }
