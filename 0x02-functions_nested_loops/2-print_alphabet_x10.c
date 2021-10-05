ad8dabb9890fdee580c89d424050e6e5d8ba0ab0
diegojojoayandun <3922@holbertonschool.com>
2021-10-05 07:17:19 -0500

    add Task2 C file to repo

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet,
+ * all characters in lowercase, ended by a new line
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+	}
+}
