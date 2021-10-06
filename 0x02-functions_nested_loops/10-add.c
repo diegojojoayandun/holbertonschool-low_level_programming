a5af9f615b43665ac1f336cca37d0c8b56627453
diegojojoayandun <3922@holbertonschool.com>
2021-10-05 19:57:22 -0500

    Add Task9, Task10 &main.h files to repo

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - function to add two numbers take from parameters
+ * @a: integer that takes the first param
+ * @b: integer that takes the second param
+ * Return: retrive a+b result
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
