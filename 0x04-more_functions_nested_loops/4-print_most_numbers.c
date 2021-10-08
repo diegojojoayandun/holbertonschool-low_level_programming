b5d715e614a92016357b9e9df07d4d1bb72e7f47
diegojojoayandun <3922@holbertonschool.com>
2021-10-07 21:34:20 -0500

    Add Task4 C File to repo

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints 0-9 numbers
+ * except 2,4
+ */
+void print_most_numbers(void)
+{
+	int n;
+
+	n = 0;
+	while (n <= 9)
+	{
+		if (n != 2 && n != 4)
+			_putchar(n + '0');
+		n++;
+	}
+	_putchar('\n');
+}
