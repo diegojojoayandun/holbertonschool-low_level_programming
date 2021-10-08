68534ca2e7ecc5ad4a331ecbb0a755e57ec3b8a8
diegojojoayandun <3922@holbertonschool.com>
2021-10-07 19:23:10 -0500

    Add Task2 C File to repo

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - funtion that multiplica two numbers taken as paramters
+ * @a: integer, first parameter
+ * @b: integer, second parameter
+ * Return: the multiplication of a*b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
