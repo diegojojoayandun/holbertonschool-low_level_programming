f6d8bdf7b329775c63af5f7f08647fdfd4fd8993
diegojojoayandun <3922@holbertonschool.com>
2021-10-08 19:47:52 -0500

    Add Task9 C File to repo

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints fizz for 3 multiples or buzz for 5
+ * Return: 0 if was success another case 1
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if (i % 3 == 0 || (i / 10) + (i % 10) == 3)
+		{
+			if (i % 5 == 0 || ((i % 10) == 0 || (i % 10) == 5))
+				printf("FizzBuzz ");
+			else
+				printf("Fizz ");
+		}
+		else if (i % 5 == 0 || ((i % 10) == 0 || (i % 10) == 5))
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+	}
+	return (0);
+}
