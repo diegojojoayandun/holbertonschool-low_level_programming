4630fa7dab399341bc79d0b2585d2ac3f737a3e7
diegojojoayandun <3922@holbertonschool.com>
2021-10-12 22:39:29 -0500

    Add Task9 C File to repo

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *_strcpy - make a copy of two strings
+ * @x: pointer as first parameter as copy of the string
+ * @y: string to be copied
+ * Return: the copied string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i, j;
+
+	j = 0;
+
+	while (x[j] != '\0')
+	{
+		j++;
+	}
+
+	for (i = 0; i < j; i++)
+	{
+		y[i] = x[i];
+	}
+	y[i] = '\0';
+
+	return (y);
+}
