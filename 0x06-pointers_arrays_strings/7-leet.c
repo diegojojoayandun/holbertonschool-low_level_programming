4787ff071f28a58c7a64e33f6ef7b524e4a9f2c8
diegojojoayandun <3922@holbertonschool.com>
2021-10-14 20:41:55 -0500

    Add Task7 C File to repo

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * leet - string in 1337
+ * @s: string to be encoded
+ * Return: string;
+ */
+char *leet(char *s)
+{
+	int i, j;
+
+	char *a = "aAeEoOtTlL";
+	char *b = "4433007711";
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+			}
+		}
+		i++;
+	}
+
+	return (s);
+}
