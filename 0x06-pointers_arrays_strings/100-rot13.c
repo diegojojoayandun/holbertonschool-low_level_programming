b121097e3df1ba290c6351850cb08b528d430f4f
diegojojoayandun <3922@holbertonschool.com>
2021-10-15 19:35:56 -0500

    Add task100 C File to repo

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * rot13 - encode to Rotate13 encryption
+ * @s: string to be encoded
+ * Return: the encode string
+ */
+char *rot13(char *s)
+{
+	int i, j;
+
+	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		j = 0;
+
+		while (a[j] != '\0')
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+				goto FIRST_LOOP;
+			}
+			j++;
+		}
+FIRST_LOOP: i++;
+	}
+	return (s);
+}
