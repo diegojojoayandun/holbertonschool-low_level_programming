d02f02376d63cad3a65c9368b5a38fecadf01841
diegojojoayandun <3922@holbertonschool.com>
2021-10-20 21:23:43 -0500

    Add Task5 C File to repo

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,21 @@
 #include "main.h"
 
 /**
+ * sqrt2 - get natural square on number n
+ * @n: number
+ * @i: iterator
+ * Return: square of n number
+ */
+int _sqrt2(int n, int i)
+{
+	if (i * i > n)
+		 return (-1);
+	if (i * i == n)
+		return (i);
+	return (_sqrt2(n, i + 1));
+}
+
+/**
  * _sqrt_recursion -  returns the natural square root of a number
  * @n: number
  * Return: Square of number n
@@ -9,20 +24,5 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	return (sqrt2(n, 1));
-}
-
-/**
- * sqrt2 - get natural square on number n
- * @n: number
- * @i: iterator
- * Return: square of n number
- */
-int sqrt2(int n, int i)
-{
-	if (i * i > n)
-		return (-1);
-	if (i * i == n)
-		return (i);
-	return (sqrt2(n, i + 1));
+	return (_sqrt2(n, 1));
 }
