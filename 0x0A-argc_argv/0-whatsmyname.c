610757020f235ded468d52212121373a0ac513ea
diegojojoayandun <3922@holbertonschool.com>
2021-10-22 06:10:13 -0500

    Add Task0 C File to repo

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name, followed by a new line
+ * @argc: Number of arguments
+ * @argv: Array of arguments recieved
+ * Return: 0 if success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
