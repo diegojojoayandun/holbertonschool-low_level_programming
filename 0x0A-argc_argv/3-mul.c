04bceaa669d61f2753b74c63ea037843947ae4b0
diegojojoayandun <3922@holbertonschool.com>
2021-10-22 06:51:43 -0500

    Add Task3 C File to repo

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -  print the result of the multiplication, followed by a new line
+ * If the program does not receive two arguments, print Error,
+ * followed by a new line, and return 1
+ * @argc: Number of arguments
+ * @argv: Array of arguments recieved
+ * Return: 0 if success
+ */
+int main(int argc, char *argv[])
+{
+	if (argc <= 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		printf("result: %d\n", atoi(argv[1]) * atoi(argv[2]));
+		return (0);
+	}
+}
