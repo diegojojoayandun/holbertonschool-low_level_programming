f7968967a064a891aa896bc77882da5f90d5ebe8
diegojojoayandun <3922@holbertonschool.com>
2021-10-22 06:15:11 -0500

    Add Task1 C File to repo

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed into it
+ * @argc: Number of arguments
+ * @argv: Array of arguments recieved
+ * Return: 0 if success
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	while (argc--)
+	{
+		i++;
+	}
+	printf("%d\n", i++);
+	return (0);
+}
