8c505120144827b3fc03d0dd0506c2c1016d56e5
diegojojoayandun <3922@holbertonschool.com>
2021-10-22 08:55:06 -0500

    Add Task4 C File to repo

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - adds positive numbers
+ * If no number is passed to the program, print 0, followed by a new line
+ * if  number contains symbols, print Error, followed by a new line, return 1
+ * @argc: Number of arguments
+ * @argv: Array of arguments recieved
+ * Return: 0 if success
+ */
+int main(int argc, char *argv[])
+{
+	int j, i, suma;
+
+	if (argc <= 2)
+	{
+		printf("0\n");
+		return (1);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			j = 0;
+			while (argv[i][j] != '\0')
+			{
+				if (!(isdigit(argv[i][j])))
+				{
+					printf("Error\n");
+					return (1);
+				}
+
+				j++;
+
+
+			}
+			suma += atoi(argv[i]);
+		}
+		printf("%d\n", suma);
+		return (0);
+	}
+}
