85559c6f097d1a7fc192dfe6f68d9ddb78eecfeb
diegojojoayandun <3922@holbertonschool.com>
2021-10-21 21:05:07 -0500

    Add Task6 C File to repo

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,31 @@
+#include "main"
+
+int prime2(int n, int i);
+
+/**
+ * is_prime_number - to know is prime number
+ * @n: number
+ * Return: 1 for prime number othercase 0
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (prime2(n, n - 1));
+}
+
+/**
+ * prime - find prime number
+ * @n: number
+ * @x: iterador
+ * Return: 1 for prime number othercase 0
+ */
+int prime2(int n, int x)
+{
+	if (x == 1)
+		return (1);
+	if (n % x == 0)
+		if (x > 0)
+			return (0);
+	return (actual_prime(n, x - 1));
+}
