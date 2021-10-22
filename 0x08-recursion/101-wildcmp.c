88dfdf0067d7fecc4c40f7665444c3d44a0b576a
diegojojoayandun <3922@holbertonschool.com>
2021-10-21 21:23:15 -0500

    Add Task8 Advance C File to repo

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * wildcmp - compare 2 strings
+ * @s1: String 1
+ * @s2: String 2 to be compare with 1
+ * Return: 1 is equal, 0 othercase
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+			return (wildcmp(s1, s2 + 1));
+		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
+			return (1);
+	}
+	return (0);
+}
