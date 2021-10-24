e32758ec0a5804471ddd2d02d3bf4477aca1b34a
diegojojoayandun <3922@holbertonschool.com>
2021-10-23 21:32:00 -0500

    update

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -20,10 +20,10 @@ int main(int argc, char *argv[])
 	else
 	{
 		_cents = atoi(argv[1]);
-		if (_cents < 0)
+		if (_cents <= 0)
 		{
 			printf("0\n");
-			return (0);
+			return (1);
 		}
 		else
 		{
