fb17cd823b3376092a0d6bae64e6f57f8686410f
diegojojoayandun <3922@holbertonschool.com>
2021-10-23 21:16:07 -0500

    Add 100 Task Advanced to repo

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the minimum number of coins
+ * to make change for an amount of money
+ * @argc: parameter counter
+ * @argv: pointer to char arguments
+ * Return: minimum numer of coins to change
+ */
+int main(int argc, char *argv[])
+{
+	int _change, _cents;
+
+	if (!(argc == 2 && argc > 1))
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		_cents = atoi(argv[1]);
+		if (_cents < 0)
+		{
+			printf("0\n");
+			return (0);
+		}
+		else
+		{
+			while (_cents > 0)
+			{
+				if (_cents % 25 < _cents)
+				{
+					_cents -= 25;
+					_change++;
+				}
+				else if (_cents % 10 < _cents)
+				{
+					_cents -= 10;
+					_change++;
+				}
+				else if (_cents % 5 < _cents)
+				{
+					_cents -= 5;
+					_change++;
+				}
+				else if (_cents % 2 < _cents)
+				{
+					_cents -= 2;
+					_change++;
+				}
+				else if (_cents % 1 < _cents)
+				{
+					_cents -= 1;
+					_change++;
+				}
+			}
+		}
+	}
+	printf("%d\n", coins);
+	return (0);
+}
