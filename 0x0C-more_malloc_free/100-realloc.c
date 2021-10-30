11af98c62123af7311dd820b419adfc9668d8d71
diegojojoayandun <3922@holbertonschool.com>
2021-10-29 22:08:49 -0500

    Add Task100 Advanced C File to repo

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,8 +10,8 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char ptr1;
-	char *old_ptr;
+	char* ptr1;
+	char old_ptr;
 	unsigned int i;
 
 	if (new_size == old_size)
