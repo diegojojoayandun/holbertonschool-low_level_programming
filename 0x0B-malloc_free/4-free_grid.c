d71ca544f4dc7e235864d28c30b33b6906dc602e
diegojojoayandun <3922@holbertonschool.com>
2021-10-29 19:40:24 -0500

    Add Task4 C File to repo

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 
 /**
- * free_grid -  frees a 2 dimensional grid previously created by your alloc_grid
+ * free_grid -  frees a 2 dimensional grid previously created by alloc_grid
  * @grid: The array to free memory
  * @height: height
  */
