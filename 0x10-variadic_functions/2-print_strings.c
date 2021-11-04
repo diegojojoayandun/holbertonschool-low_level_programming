f71f6880f4578e03c8637c22269d6a64b5ed5a5d
diegojojoayandun <3922@holbertonschool.com>
2021-11-04 14:36:17 -0500

    Hot Output from source file

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,16 +19,18 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(args, char *);
-		if (i != n - 1)
+
+		printf("%s", str);
+		if (i < n - 1 && separator != NULL)
 		{
-			if (str == NULL)
-				printf("%s%s", "(nil)", separator);
-			else
-				printf("%s%s", str, separator);
+			printf("%s", separator);
+		}
+		if (str == NULL)
+		{
+			printf("%s", "(nil)");
 		}
 
-		else if (i == n - 1)
-			printf("%s", str);
+		
 	}
 	printf("\n");
 	va_end(args);
