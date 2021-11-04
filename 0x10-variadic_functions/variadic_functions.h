a38e87dbbe44f1e1398c90e90bfe18330b65f6ef
diegojojoayandun <3922@holbertonschool.com>
2021-11-04 07:26:45 -0500

    Add main header for the project

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,10 @@
+#ifndef FUNC_VARIADIC
+#define FUNC_VARIADIC
+#include <stdlib.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
