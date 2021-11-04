e6b41a2e1f3dd1756bd84e75df27b22e7abade20
diegojojoayandun <3922@holbertonschool.com>
2021-11-04 15:43:32 -0500

    Update structure in header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,6 +2,18 @@
 #define FUNC_VARIADIC
 #include <stdlib.h>
 
+#define FORMAT(f) ("%"#f)
+/**
+ * struct print_form - Define a struct to figure out how to print
+ * @c: Character to check for
+ * @f: Pointer to function to call
+ */
+typedef struct print_form
+{
+	char *c;
+	void (*f)();
+} print_form_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
