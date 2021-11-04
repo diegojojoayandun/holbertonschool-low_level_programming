3bf6cc7092c7561dda9389d620fcddbdedd3934f
diegojojoayandun <3922@holbertonschool.com>
2021-11-04 10:46:36 -0500

    Add task1 c file to repository

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers, followed by a new line.
+ * @separator: char used to sepated numbers
+ * @n: number of argumuents
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+
+	if (n == 0 || separator == NULL)
+		exit(0);
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != n - 1)
+			printf("%d%s", va_arg(args, int), separator);
+
+		else if (i == n - 1)
+			printf("%d", va_arg(args, int));
+	}
+	printf("\n");
+	va_end(args);
+}
