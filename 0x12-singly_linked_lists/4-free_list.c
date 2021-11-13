2f3ea4b2b77179eb257889a7a4050728d618dad7
diegojojoayandun <3922@holbertonschool.com>
2021-11-12 22:02:03 -0500

    Fix errors on task4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,17 +1,19 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * free_list - frees a list_t list.
- * @head: linked list of list_t.
- *
- * Return: nothing.
+ * free_list - frees a linked list
+ * @head: list_t list to be freed
  */
 void free_list(list_t *head)
 {
+	list_t *temp;
 
-	for ( ; head != NULL; head = (*head).next)
+	while (head)
 	{
-		free((*head).str);
+		temp = head->next;
+		free(head->str);
 		free(head);
+		head = temp;
 	}
 }
