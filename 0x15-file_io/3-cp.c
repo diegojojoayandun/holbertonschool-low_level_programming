edd0fd7f45ec796037349d4141db963f6f8793ac
diegojojoayandun <3922@holbertonschool.com>
2021-11-23 13:46:28 -0500

    Fix infinite loop bug

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,36 +3,7 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <sys/types.h>
-#include <string.h>
 
-/**
- * error_handle - handle with exit error code and dprintf msg
- *
- * @f: char
- * @err_code: codigo error
- * @fd: pointer to int
- */
-void error_handle(char *f, int err_code, int *fd)
-{
-	switch (err_code)
-	{
-	case 97:
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(err_code);
-
-	case 98:
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", f);
-		exit(err_code);
-
-	case 99:
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", f);
-		exit(err_code);
-
-	case 100:
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", *fd);
-		exit(err_code);
-	}
-}
 /**
  * main - ejemplo de una funcion que toma parametros desde
  * linea de comandos
@@ -48,36 +19,60 @@ int main(int argc, char *argv[])
 	char buffer[1024];
 
 	if (argc != 3)
-		error_handle(NULL, 97, NULL);
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
 
 	filefrom = open(argv[1], O_RDONLY);
 	if (filefrom < 0)
-		error_handle(argv[1], 98, NULL);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+	}
 
 	fileto = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fileto < 0)
-		error_handle(argv[2], 99, NULL);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
 
 	r = read(filefrom, buffer, 1024);
 	if (r < 0)
-		error_handle(argv[1], 98, NULL);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
+		exit(98);
+	}
+
+
 
 	while (r > 0)
 	{
 		w = write(fileto, buffer, r);
 		if (w != r)
-			error_handle(argv[2], 98, NULL);
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
+			exit(98);
+		}
+		r = read(fileto, buffer, 1024);
 	}
 
 	c = close(fileto);
 
 	if (c < 0)
-		error_handle(NULL, 100, &fileto);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fileto);
+		exit(100);
+	}
 
 	c = close(filefrom);
 
 	if (c < 0)
-		error_handle(NULL, 100, &filefrom);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", filefrom);
+		exit(100);
+	}
+
 
 	return (0);
 }
