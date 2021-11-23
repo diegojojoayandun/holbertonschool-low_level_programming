6a6d4eb5b20ff36e9b5b1e5327b7b1a56da8b8ce
diegojojoayandun <3922@holbertonschool.com>
2021-11-23 13:56:59 -0500

    Fix some checker fails

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -23,12 +23,9 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	filefrom = open(argv[1], O_RDONLY);
 	if (filefrom < 0)
-	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-	}
 
 	fileto = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fileto < 0)
@@ -36,43 +33,33 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-
 	r = read(filefrom, buffer, 1024);
 	if (r < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
 		exit(98);
 	}
-
-
-
 	while (r > 0)
 	{
 		w = write(fileto, buffer, r);
 		if (w != r)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
-			exit(98);
+			exit(99);
 		}
 		r = read(fileto, buffer, 1024);
 	}
-
 	c = close(fileto);
-
 	if (c < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fileto);
 		exit(100);
 	}
-
 	c = close(filefrom);
-
 	if (c < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", filefrom);
 		exit(100);
 	}
-
-
 	return (0);
 }
