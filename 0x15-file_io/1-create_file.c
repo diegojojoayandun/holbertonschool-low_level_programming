cd5fc6f18d7619a33b7094b0226906355780f278
diegojojoayandun <3922@holbertonschool.com>
2021-11-23 08:53:34 -0500

    Fix Code error to pass checker, error identation

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 	int fd, s = 0;
 
 	if (filename == NULL)
-		return(-1);
+		return (-1);
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
@@ -21,16 +21,12 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	if (text_content == NULL)
-	{
-		 write(fd, text_content,0);
-
-	}
+		write(fd, text_content, 0);
 	else
 	{
 		while (text_content[s] != '\0')
 			s++;
-
-	         write(fd, text_content,s);
+		write(fd, text_content, s);
 	}
 	close(fd);
 
