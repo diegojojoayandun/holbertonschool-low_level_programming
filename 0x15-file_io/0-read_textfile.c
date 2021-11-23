90e2755b819920fa39ce030f5a7e47ead687aa5a
diegojojoayandun <3922@holbertonschool.com>
2021-11-23 08:43:22 -0500

    Add task 0 source file to repo

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads the content of a text file and print it
+ * @filename: file´s name
+ * @letters: number of leeters to read
+ *
+ * Return: the number of bytes successfully written into the file
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	int s, t;
+	char *buf;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (0);
+
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
+		return (0);
+
+	s = read(fd, buf, letters);
+	if (s < 0)
+	{
+		free(buf);
+		return (0);
+	}
+	buf[s] = '\0';
+
+	close(fd);
+
+	t = write(STDOUT_FILENO, buf, s);
+	if (t < 0)
+	{
+		free(buf);
+		return (0);
+	}
+
+	free(buf);
+	return (t);
+}
