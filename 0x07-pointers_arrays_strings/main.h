5df4b5ee850a74338e96162246a6fbbc84d1f2f7
diegojojoayandun <3922@holbertonschool.com>
2021-12-12 13:05:11 -0500

    Add Advance and cpmplte mandatory

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,4 +5,7 @@ char *_strchr(char *s, char c);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
+char *_strpbrk(char *s, char *accept);
+void set_string(char **s, char *to);
+
 int _putchar(char c);
