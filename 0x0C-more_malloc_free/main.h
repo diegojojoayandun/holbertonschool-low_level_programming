1f8c891faa7437a3e9935de2d130e5deb71697b1
diegojojoayandun <3922@holbertonschool.com>
2021-12-11 23:47:43 -0500

    Update main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,3 +6,4 @@ char *mul(char n, char *num, int num_index, char *dest, int dest_index);
 void _print(char *str, int l);
 int check_for_digits(char **av);
 void init(char *str, int l);
+int main(int argc, char *argv[]);
