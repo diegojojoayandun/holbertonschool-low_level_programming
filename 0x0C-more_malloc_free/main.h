340bad19bd7d7b94b9af94fadb3df00506045032
diegojojoayandun <3922@holbertonschool.com>
2021-12-11 23:46:55 -0500

    Update main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,3 +3,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 char *mul(char n, char *num, int num_index, char *dest, int dest_index);
+void _print(char *str, int l);
+int check_for_digits(char **av);
+void init(char *str, int l);
