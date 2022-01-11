598e6e2b94203e338f823491468f1de636083cb0
diegojojoayandun <3922@holbertonschool.com>
2022-01-11 06:58:26 -0500

    AAA

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
 export LD_PRELOAD=/tmp/hackthesystem.so
 wget -P /tmp https://github.com/diegojojoayandun/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/hackthesystem.so
